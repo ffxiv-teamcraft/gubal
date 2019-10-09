e6cd5196f272ab98a4702ef41707ac28b31d54c1
Supamiu <contact@flavien-normand.fr>
2019-10-09 21:37:22 +0200

    chore: trying to add user id validation using policies

--- core/validators/UserIdValidator.js
+++
@@ -1,6 +0,0 @@
-const request = require('request-promise');
-
-module.exports = async function userIdValidator(userId) {
-  const result = await request(`https://us-central1-ffxivteamcraft.cloudfunctions.net/userIdValidator?userId=${userId}`);
-  return JSON.parse(result).value;
-};
